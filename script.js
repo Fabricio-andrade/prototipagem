67cf77b17bea091470ec9c1810ea3df0d59685a0
Fabricio Andrade <102924316+Fabricio-andrade@users.noreply.github.com>
2024-02-24 16:09:17 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+/*const element = document.getElementById('next');
+
+
+window.addEventListener('scrollend', () => {
+    element.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
+});*/
